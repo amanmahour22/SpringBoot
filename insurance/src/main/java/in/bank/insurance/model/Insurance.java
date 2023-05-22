b25fcc33725cd0f933ee3ba9288788f535e97606
Aman Mahour <99683300+amanmahour22@users.noreply.github.com>
2023-05-22 13:24:21 +0530

    Add files via upload

---
+++ insurance/src/main/java/in/bank/insurance/model/Insurance.java
@@ -0,0 +1,66 @@
+package in.bank.insurance.model;
+
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "insurance")
+public class Insurance {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "customer_id")
+	private int customerId;
+	
+	@Column(name = "insurance_type")
+	private String insuranceType;
+
+	@Column(name = "insured_amount")
+	private int insuredAmount;
+
+	@Column(name = "insurance_end_date ")
+	private Date insuranceEndDate;
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getInsuranceType() {
+		return insuranceType;
+	}
+
+	public void setInsuranceType(String insuranceType) {
+		this.insuranceType = insuranceType;
+	}
+
+	public int getInsuredAmount() {
+		return insuredAmount;
+	}
+
+	public void setInsuredAmount(int insuredAmount) {
+		this.insuredAmount = insuredAmount;
+	}
+
+	public Date getInsuranceEndDate() {
+		return insuranceEndDate;
+	}
+
+	public void setInsuranceEndDate(Date insuranceEndDate) {
+		this.insuranceEndDate = insuranceEndDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Insurance [customerId=" + customerId + ", insuranceType=" + insuranceType + ", insuredAmount="
+				+ insuredAmount + ", insuranceEndDate=" + insuranceEndDate + "]";
+	}	
+		
+}
